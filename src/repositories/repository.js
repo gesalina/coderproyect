b24ef11411283e30edbadde9631139a56d9164bc
Gonzalo Salina <ezequiel20101942@gmail.com>
2023-08-28 08:10:44 -0300

    Update repository.js

--- src/repositories/repository.js
+++ src/repositories/repository.js
@@ -1,7 +1,12 @@
 import Auth from "../services/auth.service.js";
+
+/**
+*  Repositories
+*/
 import AuthRepository from "./auth.repository.js";
 import ProductRepository from "./product.repository.js";
 import CartRepository from "./cart.repository.js";
+import ChatRepository from './chat.repository.js';
 
 
 
