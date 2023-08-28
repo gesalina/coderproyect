b8222621a682ec4f5b28d2d7e6d3dbc189b3487f
Gonzalo Salina <ezequiel20101942@gmail.com>
2023-08-28 08:07:05 -0300

    Update repository.js

--- src/repositories/repository.js
+++ src/repositories/repository.js
@@ -1,4 +1,8 @@
 import Auth from "../services/auth.service.js";
 import AuthRepository from "./auth.repository.js";
+import ProductRepository from "./product.repository.js";
+import CartRepository from "./cart.repository.js";
+
+
 
 export const authService = new AuthRepository(new Auth());
