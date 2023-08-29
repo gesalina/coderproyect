ec3d289fd51b4a7a9ae8b91d4dd34b7d9ebc88d9
Gonzalo Salina <ezequiel20101942@gmail.com>
2023-08-29 08:06:24 -0300

    Update chat.repository.js

--- src/repositories/chat.repository.js
+++ src/repositories/chat.repository.js
@@ -2,5 +2,13 @@ export default class ChatRepository {
     constructor(dao) {
       this.dao = dao;
     }
-  
+  getMessages = async() => {
+      const result = this.dao.getMessages();
+      return result;
+  }
+
+insertMessage = async(request) => {
+    const result = this.dao.insertMessage(request);
+    return result;
+}
   }
