a25ebc1d3ec2e28fb427f1fdb8b7f840e07b411b
Gonzalo Salina <ezequiel20101942@gmail.com>
2023-08-29 08:04:19 -0300

    Add new functions

--- src/repositories/cart.repository.js
+++ src/repositories/cart.repository.js
@@ -1,6 +1,51 @@
+import ProductValidator from "../dao/dto/product.dto.js";
+
 export default class CartRepository {
   constructor(dao) {
     this.dao = dao;
+  }
+
+  getCarts = async() => {
+  const result = this.dao.getCarts();
+    return result;
+  }
+  /**
+  * The request is the cart ID
+  */
+  getCartById = async(request) => {
+    const result = this.dao.getCartById(request);
+  }
+
+  createCart = async(request) => {
+    const validate = new CartValidator(request);
+    const result = this.dao.createCart(validate);
+  }
+  /**
+  * The request is the cart ID
+  */
+  getCartProduct = async(request) => {
+    const result = this.dao.getCartProduct(request);
+    return result;
+  }
+
+  deleteProductFromCart = async(cart, product) => {
+    const result = this.dao.deleteProductFromCart(cart, product);
+    return result;
+  }
+
+  addProductCart = async(cart, product, quantity) => {
+    const result = this.dao.addProductCart(cart, product, quantity);
+    return result;
+  }
+
+  updateProductQuantity = async(cart, product, quantity) => {
+    const result = this.dao.updateProductQuantity(cart, product, quantity);
+    return result;
+  }
+
+  deleteAllProducts = async(cart) => {
+    const result = this.dao.deleteAllProducts(cart);
+    return result;
   }
 
 }
