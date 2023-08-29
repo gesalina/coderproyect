d3ebe76b0c7d6b25863a1b8e667105b8acc35ae6
Gonzalo Salina <ezequiel20101942@gmail.com>
2023-08-29 13:55:01 -0300

    Update carts.controller.js

--- src/controllers/carts.controller.js
+++ src/controllers/carts.controller.js
@@ -1,9 +1,10 @@
+import {cartRepository} from "../repositories/repository.js";
 import Cart from "../services/cart.service.js";
 const cartService = new Cart();
 
 export const getCartsController = async (request, response) => {
   try {
-    const result = await cartService.getCarts();
+    const result = await cartRepository.getCarts();
     return response.status(200).json(result);
   } catch (error) {
     return response.status(404).json({ status: "error", error: error });
@@ -13,7 +14,7 @@ export const getCartsController = async (request, response) => {
 export const getCartProductsController = async (request, response) => {
   const cartId = request.params.cid;
   try {
-    const result = await cartService.getCartProduct(cartId);
+    const result = await cartRepository.getCartProduct(cartId);
     if (result.error) {
       return response
         .status(404)
@@ -28,7 +29,7 @@ export const getCartProductsController = async (request, response) => {
 export const findCartByIdController = async (request, response) => {
   const cartId = request.params.cid;
   try {
-    const result = await cartService.findCartById(cartId);
+    const result = await cartRepository.findCartById(cartId);
     if (result.error) {
       return response
         .status(404)
@@ -43,7 +44,7 @@ export const findCartByIdController = async (request, response) => {
 export const createCartController = async (request, response) => {
   const {userId} = request.body;
   try {
-    const result = await cartService.createCart(userId);
+    const result = await cartReposity.createCart(userId);
     response.json({ message: "Carrito creado satisfactoriamente" });
   } catch (error) {
     return response.status(404).json({ status: "error", error: error });
@@ -54,7 +55,7 @@ export const deleteProductController = async (request, response) => {
   const cartId = request.params.cid;
   const productId = request.params.pid;
   try {
-    const result = await cartService.deleteProductFromCart(cartId, productId);
+    const result = await cartRepository.deleteProductFromCart(cartId, productId);
     if (result.error) {
       return response
         .status(404)
@@ -70,7 +71,7 @@ export const updateCartController = async (request, response) => {
   const cartId = request.params.cid;
   const { product, quantity } = request.body;
   try {
-    const result = await cartService.addProductCart(cartId, product, quantity);
+    const result = await cartRepositoy.addProductCart(cartId, product, quantity)
     if (result.error) {
       return response
         .status(404)
@@ -87,7 +88,7 @@ export const updateProductController = async (request, response) => {
   const productId = request.params.pid;
   const { quantity } = request.body;
   try {
-    const result = await cartService.updateProductQuantity(cartId, productId, quantity);
+    const result = await cartReposity.updateProductQuantity(cartId, productId, quantity)
     if (result.error) {
       return response
         .status(404)
@@ -102,7 +103,7 @@ export const updateProductController = async (request, response) => {
 export const emptyCartProductController = async (request, response) => {
   const cartId = request.params.cid;
   try {
-    const result = await cartService.deleteAllProducts(cartId);
+    const result = await cartRepository.deleteAllProducts(cartId);
     if (result.error || result.modifiedCount == 0) {
       return response
         .status(404)
