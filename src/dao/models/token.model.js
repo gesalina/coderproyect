f32a83103985b47e29706c4d432fad03d12109c8
Gonzalo Salina <ezequiel20101942@gmail.com>
2023-09-23 10:10:57 -0300

    Update token.model.js

--- src/dao/models/token.model.js
+++ src/dao/models/token.model.js
@@ -1,1 +1,24 @@
+import mongoose from "mongoose";
 
+const tokenCollection = "token";
+
+const tokenSchema = new mongoose.Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "users"
+  },
+  token: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 3600,
+  }
+});
+
+const tokenModel = mongoose.model(tokenCollection, tokenSchema);
+
+export default tokenModel;
